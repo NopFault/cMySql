c1f56c7ed5df61239cd88429bc979c807f7f090f
nopFault <nopfault@gmail.com>
2022-01-04 22:17:36 +0200

    Make logic for file read

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+// Maybe to model files or something
+typedef struct Database {
+    char hostname[50];
+    char login[50];
+    char pass[50];
+} Database;
+
+Database * hostList();
 void loginScreen();
 
 int main(void)
@@ -16,24 +24,37 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
-// move to external file
+// Generate Database typedef with data from file
+Database * hostList( Database * hosts )
+{
+    // From file to hosts array
+    for (int x=0; x<=5; x++) {
+        hosts[x] = (struct Database){"rferf","erferf","erferfe"};
+    }
+
+    return hosts;
+}
+
 void loginScreen()
 {
 
-    // Read from file
-    char hosts[][10] = {"host_1", "host_2", "host_3", "host_4", "host_5"};
-    char host[10];
+    // Read from file and generate this:
+    Database h[100];
+    Database * hosts = hostList(h);
+
+    char host[50];
     int ch, i = 0;
 
     int yMax, xMax;
     getmaxyx(stdscr,yMax, xMax);
 
     // create a window for input
-    WINDOW * login_win = newwin(9, xMax-12, yMax / 2 - 5, 8);
+    WINDOW * login_win = newwin(sizeof(hosts)+2, xMax-12, yMax / 2 - 5, 8);
     box(login_win, 0,0);
     mvwprintw(login_win, 0, 2, "  Select host: ");
-    for (i=0; i<=4; i++) {
-        sprintf(host, "%s", hosts[i]);
+
+    for (i=0; i<=(sizeof(hosts)-1); i++) {
+        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
         mvwprintw(login_win, i+2, 2, host);
     }
 
@@ -44,25 +65,29 @@ void loginScreen()
 
     // app loop
     while((ch = wgetch(login_win)) != 'q') {
-        sprintf(host, "%s",  hosts[i]);
-        mvwprintw( login_win, i+2, 2, "%s", host );
+        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
+        mvwprintw( login_win, i+2, 2, "%.*s", (int)sizeof(host), host );
 
         // event keys
         switch( ch ) {
             case KEY_UP:
                 i--;
-                i = (i<0) ? 4 : i;
+                i = (i<0) ? (sizeof(hosts)-3) : i;
                 break;
             case KEY_DOWN:
                 i++;
-                i = (i > 4) ? 0 : i;
+                i = (i >= (sizeof(hosts)-2)) ? 0 : i;
+                break;
+            case KEY_ENTER:
+                // Make new window alert
                 break;
         }
+
         wattron(login_win, A_STANDOUT);
-        sprintf(host, "%s",  hosts[i]);
-        mvwprintw( login_win, i+2, 2, "%s", host );
+        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
+        mvwprintw( login_win, i+2, 2, "%.*s", (int)sizeof(host), host );
         wattroff(login_win, A_STANDOUT);
-        /* refresh(); */
+        refresh();
         wrefresh(login_win);
     }
 

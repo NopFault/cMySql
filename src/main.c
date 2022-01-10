8b6714fd15de2c881e640a833a870f6d7ed0594e
nopFault <nopfault@gmail.com>
2022-01-10 22:08:48 +0200

    Learning pointers on structures of array

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PER_PAGE 5
 
 // Maybe to model files or something
-typedef struct Database {
+typedef struct {
     char hostname[50];
     char login[50];
     char pass[50];
 } Database;
 
-Database * hostList();
+void hostList();
 void loginScreen();
 
 int main(void)
@@ -25,37 +28,34 @@ int main(void)
 }
 
 // Generate Database typedef with data from file
-Database * hostList( Database * hosts )
+void hostList(Database * hosts)
 {
     // From file to hosts array
-    for (int x=0; x<=5; x++) {
-        hosts[x] = (struct Database){"rferf","erferf","erferfe"};
+    for (int x = 0; x < PER_PAGE; ++x) {
+        hosts[x] = (Database){"hostname", "loginname", "password"};
     }
-
-    return hosts;
 }
 
 void loginScreen()
 {
 
     // Read from file and generate this:
-    Database h[100];
-    Database * hosts = hostList(h);
+    Database hosts[PER_PAGE];
+    hostList(hosts);
 
-    char host[50];
     int ch, i = 0;
+    int size_of_hosts = sizeof(hosts) / sizeof(Database);
 
     int yMax, xMax;
     getmaxyx(stdscr,yMax, xMax);
 
     // create a window for input
-    WINDOW * login_win = newwin(sizeof(hosts)+2, xMax-12, yMax / 2 - 5, 8);
+    WINDOW * login_win = newwin(size_of_hosts*2, xMax-12, yMax / 2 - 5, 8);
     box(login_win, 0,0);
     mvwprintw(login_win, 0, 2, "  Select host: ");
 
-    for (i=0; i<=(sizeof(hosts)-1); i++) {
-        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
-        mvwprintw(login_win, i+2, 2, host);
+    for (int x = 0; x < size_of_hosts; ++x) {
+        mvwprintw(login_win, x+2, 2, hosts[x].hostname);
     }
 
     keypad(login_win, TRUE);
@@ -65,18 +65,17 @@ void loginScreen()
 
     // app loop
     while((ch = wgetch(login_win)) != 'q') {
-        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
-        mvwprintw( login_win, i+2, 2, "%.*s", (int)sizeof(host), host );
+        mvwprintw( login_win, i+2, 2, "%.*s", (int)strlen(hosts[i].hostname), hosts[i].hostname);
 
         // event keys
         switch( ch ) {
             case KEY_UP:
                 i--;
-                i = (i<0) ? (sizeof(hosts)-3) : i;
+                i = (i < 0) ? (size_of_hosts) : i;
                 break;
             case KEY_DOWN:
                 i++;
-                i = (i >= (sizeof(hosts)-2)) ? 0 : i;
+                i = (i >= size_of_hosts) ? 0 : i;
                 break;
             case KEY_ENTER:
                 // Make new window alert
@@ -84,8 +83,7 @@ void loginScreen()
         }
 
         wattron(login_win, A_STANDOUT);
-        sprintf(host, "%.*s",  (int)sizeof(hosts[i].hostname), hosts[i].hostname);
-        mvwprintw( login_win, i+2, 2, "%.*s", (int)sizeof(host), host );
+        mvwprintw( login_win, i+2, 2, "%.*s", (int)strlen(hosts[i].hostname), hosts[i].hostname);
         wattroff(login_win, A_STANDOUT);
         refresh();
         wrefresh(login_win);
